fb53dad26d1131e5db13db3ff43029629ce418c8
Takashi Nasu <nasrinjp@gmail.com>
2021-04-13 09:19:52 +0900

    20.ルート定義の分割

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
+import { Router } from "./router/Router";
 import "./styles.css";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
@@ -17,30 +18,7 @@ export default function App() {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/page1"
-          render={({ match: { url } }) => (
-            <Switch>
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
