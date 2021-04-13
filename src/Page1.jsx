659141c026cc16616caedd84b4f215c9eb71a269
Takashi Nasu <nasrinjp@gmail.com>
2021-04-13 09:58:55 +0900

    23.Linkを使わないページ遷移（useHistory）

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
   console.log(arr);
+
+  const history = useHistory();
+
+  const onClickDetailA = () => history.push("/page1/detailA");
+
   return (
     <div>
       <h1>Page1です</h1>
       <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailAです</Link>
       <br />
       <Link to="/page1/detailB">DetailBです</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
